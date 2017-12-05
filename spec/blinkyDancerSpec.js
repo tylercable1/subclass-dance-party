2b18806642eb11f9c9459f1110d19316bcfdafa9
tylercable1 <tylercable7@gmail.com>
2017-12-05 15:25:42 -0800

    wrote tests

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -18,6 +18,15 @@ describe('blinkyDancer', function() {
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
 
+  it('has lineUp method', function() {
+    expect(typeof blinkyDancer.lineUp === 'function').to.be.true;
+  });
+
+  it('has danceOff method', function() {
+    expect(typeof blinkyDancer.danceOff === 'function').to.be.true;
+  });
+
+
   describe('dance', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
@@ -31,4 +40,83 @@ describe('blinkyDancer', function() {
       expect(blinkyDancer.step.callCount).to.be.equal(2);
     });
   });
+});
+
+
+describe('growingDancer', function () {
+
+  var growingDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    growingDancer = new makeGrowingDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('has lineUp method', function() {
+    expect(typeof growingDancer.lineUp === 'function').to.be.true;
+  });
+
+  it('has danceOff method', function() {
+    expect(typeof growingDancer.danceOff === 'function').to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(growingDancer, 'step');
+      expect(growingDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(growingDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(growingDancer.step.callCount).to.be.equal(2);
+    });
+  });
+
+
+
+
+
+});
+
+
+
+describe('crappyDancer', function () {
+
+  var crappyDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    crappyDancer = new makeCrappyDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('has lineUp method', function() {
+    expect(typeof crappyDancer.lineUp === 'function').to.be.true;
+  });
+
+  it('does NOT have danceOff method', function() {
+    expect(typeof crappyDancer.danceOff === 'function').to.be.false;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(crappyDancer, 'step');
+      expect(crappyDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+
+      expect(crappyDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(crappyDancer.step.callCount).to.be.equal(2);
+    });
+  });
+
+
+
+
+
 });
